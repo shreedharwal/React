85351f27d5101c9c32c13c24d14f5b453e633ee6
shreya dharwal <107870585+shreedharwal@users.noreply.github.com>
2025-06-05 15:20:07 +0000

    updated

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -9,7 +9,7 @@ function customRender(reactElement,container){
     const domElement= document.createElement(reactElement.type)
     domElement.innerHTML= reactElement.children
     for (const prop in reactElement.props) {
-        if(prop == children) continue;
+        if(prop == 'children') continue;
         domElement.setAttribute(prop, reactElement.props[prop])
             
         }
