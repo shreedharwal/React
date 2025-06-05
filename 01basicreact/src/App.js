7bb572d291ebc9919272e95133e13dc77632f0c0
shreya dharwal <107870585+shreedharwal@users.noreply.github.com>
2025-06-05 07:01:20 +0000

    Updated 01_basicreact

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,5 +1,3 @@
-
-
 function App() {
   return (
     <h1>Basics of React</h1>
--- 01basicreact/src/index.js
+++ 01basicreact/src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react';//core foundational lib which take all the references
+import ReactDOM from 'react-dom/client';//to manipulate web dom, implementation
 
 import App from './App';
 
@@ -7,7 +7,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>
 );
 
+// <App /> is a custom tag
