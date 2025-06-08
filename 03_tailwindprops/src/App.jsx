1f951ed9a3e736a414e4fadd6e4b6de20925ebcb
shreya dharwal <107870585+shreedharwal@users.noreply.github.com>
2025-06-08 10:38:02 +0000

    About props

--- 03_tailwindprops/src/App.jsx
+++ 03_tailwindprops/src/App.jsx
@@ -1,3 +1,4 @@
+// props make content reusable
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -8,7 +9,62 @@ function App() {
 
   return (
     <>
-      <h1>Tailwind test</h1>
+      <h1 className='bg-green-400 p-4 rounded-2xl'>Tailwind test</h1>
+      {/* <div class="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
+  <div>
+    <img class="size-48 shadow-xl rounded-md" alt="" src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+  </div>
+  <div class="flex items-center md:items-start">
+    <span class="text-2xl font-medium">Class Warfare</span>
+    <span class="font-medium text-sky-500">The Anti-Patterns</span>
+    <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+      <span>No. 4</span>
+      <span>·</span>
+      <span>2025</span>
+    </span>
+  </div> 
+ </div> */}
+
+
+
+
+          <div className="flex flex-col rounded-xl  p-4"
+        style={{
+          border: '0.88px solid',
+  
+          backdropFilter: 'saturate(180%) blur(14px)',
+          background: ' #ffffff0d',
+        }}
+      >
+        <div>
+          <img
+            src="https://res.cloudinary.com/ddcg0rzlo/image/upload/v1652470298/9StaF0UBJfih_df0248.gif"
+            alt="nft-gif"
+            width="100"
+            height="100"
+            className="rounded-xl"
+          />
+        </div>
+        <div className="flex flex-col  rounded-b-xl py-4 ">
+          <div className="flex justify-between">
+            <h1 className="font-RubikBold ">Bored ape nft accidental</h1>
+            <h1 className="font-bold font-RubikBold">Price</h1>
+          </div>
+          <div className="flex  justify-between font-mono">
+            <p>#345</p>
+            <p>0.01</p>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
     </>
   )
 }
