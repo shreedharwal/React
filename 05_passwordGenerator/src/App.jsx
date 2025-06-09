b865b2dab51a0a8c0cf6f306971475ee58a612aa
shreya dharwal <107870585+shreedharwal@users.noreply.github.com>
2025-06-09 10:49:01 +0000

    updated

--- 05_passwordGenerator/src/App.jsx
+++ 05_passwordGenerator/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useCallback } from 'react'
-import './App.css'
+
 
 function App() {
   const [length, setLength] = useState(8)
@@ -14,11 +14,32 @@ function App() {
     if(numAllowed) str += '0123456789'
     if(charAllowed) str += '!@#$%^&*()-_=+{}~'
 
+    for (let i = 1; i < array.length; i++) {
+      let char = Math.floor(Math.random()*str.length +1)
+      pass= str.charAt(char)
+    }
+
+    setpassword(pass)
+
   }, [length, numAllowed, charAllowed, setpassword])
 
   return (
     <>
-      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-gray-300'>
+        <h1 className='text-white text-center my-3'>Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input 
+          type='text'
+          value={password} 
+          className='outline-none w-full py-1 px-3 bg-gray-200 text-gray-900'
+          placeholder='password'
+          readOnly/>
+
+        <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+        >Copy</button>
+          
+        </div>
+      </div>
     </>
   )
 }
