6b735e301b87288f4deffb935e186b4d78a99c4a
shreya dharwal <107870585+shreedharwal@users.noreply.github.com>
2025-06-10 05:37:35 +0000

    06_currencyChanger project start

---
+++ 06_currencyChanger/src/hooks/useCurrencyinfo.js
@@ -0,0 +1,9 @@
+import { useEffect,useState } from "react";
+
+function useCurrencyinfo(currency){
+    useEffect(()=>{
+        fetch()
+    }, [])
+}
+
+url='https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json'
