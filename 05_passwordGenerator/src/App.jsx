404627e8e563d4f4e4f0de3f7c10f4ae9e55ae68
shreya dharwal <107870585+shreedharwal@users.noreply.github.com>
2025-06-10 05:15:17 +0000

    To be continued

--- 05_passwordGenerator/src/App.jsx
+++ 05_passwordGenerator/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     if(numAllowed) str += '0123456789'
     if(charAllowed) str += '!@#$%^&*()-_=+{}~'
 
-    for (let i = 1; i < array.length; i++) {
+    for (let i = 1; i < length; i++) {
       let char = Math.floor(Math.random()*str.length +1)
       pass= str.charAt(char)
     }
@@ -24,7 +24,7 @@ function App() {
   }, [length, numAllowed, charAllowed, setpassword])
 
   return (
-    <>
+    
       <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-gray-300'>
         <h1 className='text-white text-center my-3'>Password Generator</h1>
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
@@ -33,27 +33,33 @@ function App() {
           value={password} 
           className='outline-none w-full py-1 px-3 bg-gray-200 text-gray-900'
           placeholder='password'
-          readOnly/>
+          readOnly
+          ref={passwordRef}
+          />
 
-        <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+        <button 
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
         >Copy</button>
-          
-        <div className='flex text-sm gap-x-2'>
-          <div classname='flex items-center gap-x-1'>
-            <input
-            type='range'
-            min={6}
-            max={100}
-            value={length}
-            classname='cursor-pointer'
-            />
-            <label>Length: {length}</label>
-          </div>
-        </div>
-        </div>
+        </div> 
+        
+      <div className='flex text-sm gap-x-2'>
+      <div className='flex items-center gap-x-1'>
+        <input 
+        type="range"
+        min={6}
+        max={100}
+        value={length}
+         className='cursor-pointer'
+         onChange={(e) => {setLength(e.target.value)}}
+        />
+          <label>Length: {length}</label>
       </div>
-    </>
+      </div>
+    
   )
 }
 
 export default App
+
+
+{/* to be continued from here */}
